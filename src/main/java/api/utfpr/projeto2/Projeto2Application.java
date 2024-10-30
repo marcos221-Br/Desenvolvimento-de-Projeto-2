a2e5f99244bc71d567a895b771d6868de325f623
Marcos Gomes <marcosartemio221@gmail.com>
2024-10-29 23:52:09 -0300

    Bug Fix
    Software didn't start due the missing bean StorageConfig

--- src/main/java/api/utfpr/projeto2/Projeto2Application.java
+++ src/main/java/api/utfpr/projeto2/Projeto2Application.java
@@ -3,11 +3,14 @@ package api.utfpr.projeto2;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+import api.utfpr.projeto2.configs.StorageConfig;
 import api.utfpr.projeto2.services.MessageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageConfig.class)
 public class Projeto2Application {
 
 	public static void main(String[] args) {
